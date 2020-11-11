be91adb78c970852f6e97010ed254c285a6a1bc1
tomatis55 <joskarkarlsson@gmail.com>
2020-11-11 13:31:18 +0100

    hibuy

--- phptest.php
+++ phptest.php
@@ -3,11 +3,7 @@
 <body>
 
 <?php
-echo "<h2>PHP is Fun!</h2>";
 echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
-
 $servername = "utbweb.its.ltu.se";
 $username = "990815";
 $password = "990815";
@@ -22,14 +18,15 @@ if (!$conn) {
 }
 echo "Connected successfully<br>";
 
-$sql = "SELECT * FROM test";
-
+// Test printing a selection from database
+$sql = "SELECT age FROM test WHERE name='oskar'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "NAMN: " . $row["name"]. " ÅLDER: " . $row["age"]. " HEMSTAD: " . $row["home"]. "<br>";
+    //echo "NAMN: " . $row["name"]. " ÅLDER: " . $row["age"]. " HEMSTAD: " . $row["home"]. "<br>";
+    echo "age: " . $row["age"]. "<br>";
   }
 } else {
   echo "0 results";
