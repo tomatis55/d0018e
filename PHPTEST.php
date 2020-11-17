99acd0d9cf0d8c07841cfb049148c7a4f81afbda
Cloud327 <roman.ermolaev@hotmail.com>
2020-11-17 11:27:44 +0100

    r

--- PHPTEST.php
+++ PHPTEST.php
@@ -1,17 +1,13 @@
-<?php
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-?>
+
 <html>
 <body>
 
 <?php
-if(!isset($_COOKIE[$cookie_name])) {
-  echo "Cookie named '" . $cookie_name . "' is not set!";
+if(!isset($_COOKIE["user"])) {
+  echo "Cookie named '" . "user". "' is not set!";
 } else {
-  echo "Cookie '" . $cookie_name . "' is set!<br>";
-  echo "Value is: " . $_COOKIE[$cookie_name];
+  echo "Cookie '" . "user". "' is set!<br>";
+  echo "Value is: " . $_COOKIE["user"];
 }
 ?>
 
