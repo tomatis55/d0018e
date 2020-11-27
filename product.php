acac83409bb7e7254e306b94a3f83a09b1561521
tomatis55 <joskarkarlsson@gmail.com>
2020-11-27 11:58:31 +0100

    fix

--- product.php
+++ product.php
@@ -27,23 +27,6 @@ if($_COOKIE["user"] == '0'){?>
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-if(!isset($_COOKIE['user'])){
-  
-
-    $sql = "SELECT * FROM Kunder ORDER BY Kundnr DESC LIMIT 1";
-    $result = $conn->query($sql);
-    $x = $result->fetch_assoc();
-
-
-    $cookie_name = "user";
-    $cookie_value = $x[Kundnr];
-    setcookie($cookie_name, $cookie_value, time() + (86400*30), "/"); // 86400 = 1 day
-
-    $sql = "INSERT INTO `Kunder`() VALUES ()";
-    $result = $conn->query($sql);
-        
-
-}
 
 ?>
 
